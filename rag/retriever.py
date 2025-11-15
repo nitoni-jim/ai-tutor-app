8411304e2717df2a54aa893a0215e291fe9eb0cc
nitoni-jim <nitoni4fj@gmail.com>
2025-11-15 14:26:15 +0100

    Update retriever.py

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,2 +1,13 @@
+# retriever.py
+import numpy as np
+from vector_store import cosine_similarity
+
 class Retriever:
-    pass
+    def __init__(self, vectors, texts):
+        self.vectors = vectors
+        self.texts = texts
+
+    def retrieve(self, query_vec, top_k=5):
+        similarities = np.array([cosine_similarity(query_vec, v) for v in self.vectors])
+        idxs = similarities.argsort()[::-1][:top_k]
+        return [(self.texts[i], similarities[i]) for i in idxs]
